f6d096d62c8c2c708164305559ad7740653f443a
Shikha Gupta <shiknema@gmail.com>
2021-01-30 14:55:26 +0100

    changed API call method

--- frontend/src/component/truckdriver.js
+++ frontend/src/component/truckdriver.js
@@ -11,15 +11,18 @@ class TruckDriver extends Component { //class component
             truckdriver: ''
         }
     }
-    componentDidMount(){ //lifecycle event hook. Called automatically once this component is mounted on the view
-        fetch(url,{method:'GET'})  //fetch calls the json-server REST API
-        .then((response) => response.json()) //if success, retuns an async Promise with the data from the service as response obj
-        .then((data) => {  //data is auto populated by the fetch method with data from the REST service
-            this.setState({ //sets the data retreived from the service to the local props via the state
-               truckDrivers:data
-            })
-        })
-    }
+   
+
+    async componentDidMount() { //lifecycle event hook using async function for making a get request. Called automatically once this component is mounted on the view
+        this.setState({ isLoading: true }) // for loding perpose
+        const response = await fetch('http://localhost:6700/truckdrivers') //fetch calls the json-server REST API
+        if (response.ok) { // if response recived 
+          const truckDrivers = await response.json() //await used because returned from API take some time, //data is auto populated by the fetch method with data from the REST service
+          this.setState({ truckDrivers, isLoading: false })//if success, retuns an async Promise with the data from the service as response obj
+        } else {                                            //sets the data retreived from the service to the local props via the state
+          this.setState({ isError: true, isLoading: false })// otherwise it retun Error
+        }
+      }
     render(){ //display this component in the view
         console.log(this.state.truckDrivers); //print products on the console of the browser
         return( //display title and inject ProductsList component here by passing the products data to it
